8f36b48eeb74403acc98f51246c5d42444adb656
pwachira900@gmail.com <pwachira900@gmail.com>
2019-02-21 07:21:50 +0300

    added the form and the submit form

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $("#blanks form").submit(function(event) {
+      var blanks = ["noun1", "noun2", "adjective1", "name1", "adjective2", "verb1","bodypart1","verb2"]
+
+      blanks.forEach(function(blank) {
+        var userInput = $("input#" + blank).val();
+        $("." + blank).text(userInput);
+      });
+
+      $("#story").show();
+
+      event.preventDefault();
+    });
+  });
+
+  ;
