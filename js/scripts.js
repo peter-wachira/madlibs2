d4ba1f007e69ca395c172485e4cfdcd1e4e9dba5
pwachira900@gmail.com <pwachira900@gmail.com>
2019-02-23 11:15:45 +0300

    include jquery and javascript code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $("#blanks form").submit(function(event) {
-      var blanks = ["noun1", "noun2", "adjective1", "name1", "adjective2", "verb1","bodypart1","verb2"]
+      var blanks = ["noun1", "noun2", "adjective1", "name1", "adjective2", "verb1","bodypart1","verb2"];
 
       blanks.forEach(function(blank) {
         var userInput = $("input#" + blank).val();
